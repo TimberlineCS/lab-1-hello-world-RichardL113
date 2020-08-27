b051b78958ae604976453f2c24514d45ba43adb0
RichardL113 <rle111303@gmail.com>
2020-08-27 15:52:50 +0000

    stuff

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//Richard Le
+//AP computer science
+//24-8-2020
+public class HelloWorld.java {
+
+    public static void main(String[] args) {
+
+        System.out.println("▒▒▄▀▀▀▀▀▄▒▒▒▒▒▄▄▄▄▄▒▒▒");
+        System.out.println("▒▐░▄░░░▄░▌▒▒▄█▄█▄█▄█▄▒");
+        System.out.println("▒▐░▀▀░▀▀░▌▒▒▒▒▒░░░▒▒▒▒");
+        System.out.println("▒▒▀▄░═░▄▀▒▒▒▒▒▒░░░▒▒▒▒");
+        System.out.println("▒▒▐░▀▄▀░▌▒▒▒▒▒▒░░░▒▒▒▒");
+
+    }
+
+}
