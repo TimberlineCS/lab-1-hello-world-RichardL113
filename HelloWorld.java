845a474b6bef21fbd7b9f030ebf71e4806177ae3
RichardL113 <rle111303@gmail.com>
2020-08-27 15:53:19 +0000

    yes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 //24-8-2020
 public class HelloWorld.java {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         System.out.println("▒▒▄▀▀▀▀▀▄▒▒▒▒▒▄▄▄▄▄▒▒▒");
         System.out.println("▒▐░▄░░░▄░▌▒▒▄█▄█▄█▄█▄▒");
